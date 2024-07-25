ae194d3db49b7a6bad803139d41d7d91d7095dd8
rsysganeshpatwa <ganesh.patwa@rsystems.com>
2024-07-25 15:03:47 +0530

    code push

---
+++ android/app/src/main/kotlin/com/example/video_meeting_room/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.video_meeting_room
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/widgets/no_video.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+import 'package:video_meeting_room/theme.dart';
+
+class NoVideoWidget extends StatelessWidget {
+  //
+  const NoVideoWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) => Container(
+        alignment: Alignment.center,
+        child: LayoutBuilder(
+          builder: (ctx, constraints) => Icon(
+            Icons.videocam_off_outlined,
+            color: LKColors.lkBlue,
+            size: math.min(constraints.maxHeight, constraints.maxWidth) * 0.3,
+          ),
+        ),
+      );
+}
