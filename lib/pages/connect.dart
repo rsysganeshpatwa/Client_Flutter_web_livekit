4edd0a95846adaed576aa2e667346c859a2a5bbf
rsysganeshpatwa <ganesh.patwa@rsystems.com>
2024-08-02 11:23:33 +0530

    code change room list added

--- lib/pages/connect.dart
+++ lib/pages/connect.dart
@@ -17,14 +17,11 @@ import 'package:video_meeting_room/pages/prejoin.dart';
 import 'package:video_meeting_room/widgets/text_field.dart';
 
 import '../exts.dart';
-
-
+import 'package:flutter/src/widgets/async.dart' as asyncstate;
 
 class ConnectPage extends StatefulWidget {
+  const ConnectPage({super.key});
 
-    const ConnectPage({
-    super.key,
-  });
   @override
   State<StatefulWidget> createState() => _ConnectPageState();
 }
@@ -32,19 +29,26 @@ class ConnectPage extends StatefulWidget {
 class _ConnectPageState extends State<ConnectPage> {
   static const _storeKeyIdentity = 'identity';
 
+  final  tokenServiceUrl = 'https://token-livekit-service.onrender.com/token';
+
   final _identityCtrl = TextEditingController();
+
+  final _roomCtrl = TextEditingController();
 
   bool _busy = false;
 
+  String? roomNameFromUrl;
+
+  bool _isRoomNameInUrl = false;
+  late final activeRoomNames;
   @override
   void initState() {
     super.initState();
 
     _readPrefs();
-    
 
     if (lkPlatformIs(PlatformType.android)) {
-      _checkPremissions();
+      _checkPermissions();
     }
   }
 
@@ -52,10 +56,12 @@ class _ConnectPageState extends State<ConnectPage> {
   void dispose() {
     _identityCtrl.dispose();
 
+    _roomCtrl.dispose();
+
     super.dispose();
   }
 
-  Future<void> _checkPremissions() async {
+  Future<void> _checkPermissions() async {
     var status = await Permission.bluetooth.request();
 
     if (status.isPermanentlyDenied) {
@@ -81,20 +87,42 @@ class _ConnectPageState extends State<ConnectPage> {
     }
   }
 
-  // Read saved Identity
-
   Future<void> _readPrefs() async {
     final prefs = await SharedPreferences.getInstance();
 
     _identityCtrl.text = prefs.getString(_storeKeyIdentity) ?? '';
-  }
 
-  // Save Identity
+    setState(() {
+      roomNameFromUrl = Uri.base.queryParameters[
+          'room']; // Replace with logic to get room name from URL if applicable
+
+      if (roomNameFromUrl != null) {
+        _roomCtrl.text = roomNameFromUrl!;
+        _isRoomNameInUrl = true;
+      }
+    });
+  }
 
   Future<void> _writePrefs() async {
     final prefs = await SharedPreferences.getInstance();
 
     await prefs.setString(_storeKeyIdentity, _identityCtrl.text);
+  }
+
+  Stream<List<String>> getRoomList() async* {
+    while (true) {
+      final response = await http.get(Uri.parse(tokenServiceUrl));
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body) as List;
+        final roomList = data.map((room) => room['name'].toString()).toList();
+        print('Room List: $roomList');
+        yield roomList;
+      }
+
+      // Wait for a while before the next request
+      await Future.delayed(Duration(seconds: 5));
+    }
   }
 
   Future<void> _connect(BuildContext ctx) async {
@@ -107,18 +135,19 @@ class _ConnectPageState extends State<ConnectPage> {
 
       await _writePrefs();
 
-
-// Load the .env file
-      
+      // Load the .env file
 
       final identity = _identityCtrl.text;
+
+      final roomName = _roomCtrl.text;
 
       // Call the Node.js API to get the token
 
       final response = await http.post(
-        Uri.parse('http://localhost:3000/token'),
+        //https://token-livekit-service.onrender.com/token
+        Uri.parse(tokenServiceUrl),
         headers: {'Content-Type': 'application/json'},
-        body: json.encode({'identity': identity, 'roomName': 'room1'}),
+        body: json.encode({'identity': identity, 'roomName': roomName}),
       );
 
       if (response.statusCode == 200) {
@@ -128,7 +157,7 @@ class _ConnectPageState extends State<ConnectPage> {
 
         print('Connecting with identity: $identity, token: $token...');
 
-       var url = 'wss://poc-test-7otdfht1.livekit.cloud'; 
+        var url = 'wss://poc-test-7otdfht1.livekit.cloud';
 
         await Navigator.push<void>(
           ctx,
@@ -167,57 +196,127 @@ class _ConnectPageState extends State<ConnectPage> {
           title: Text('POC Health Care Monitoring'),
           backgroundColor: Colors.white,
         ),
-        body: Container(
-          alignment: Alignment.center,
-          child: SingleChildScrollView(
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20,
-                vertical: 20,
-              ),
-              constraints: const BoxConstraints(maxWidth: 400),
-              child: Column(
-                mainAxisSize: MainAxisSize.min,
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 70),
-                    child: SvgPicture.asset(
-                      'images/logo-dark.svg',
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 25),
-                    child: LKTextField(
-                      label: 'Name',
-                      ctrl: _identityCtrl,
-                    ),
-                  ),
-                  ElevatedButton(
-                    onPressed: _busy ? null : () => _connect(context),
-                    child: Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        if (_busy)
-                          const Padding(
-                            padding: EdgeInsets.only(right: 10),
-                            child: SizedBox(
-                              height: 15,
-                              width: 15,
-                              child: CircularProgressIndicator(
-                                color: Colors.white,
-                                strokeWidth: 2,
-                              ),
-                            ),
+        body: Stack(
+          children: [
+            Container(
+              alignment: Alignment.center,
+              child: SingleChildScrollView(
+                child: Container(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  constraints: const BoxConstraints(maxWidth: 400),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 25),
+                        child: SvgPicture.asset('images/logo-dark.svg'),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 25),
+                        child: LKTextField(
+                          label: 'Name',
+                          ctrl: _identityCtrl,
+                        ),
+                      ),
+                      if (!_isRoomNameInUrl)
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 25),
+                          child: LKTextField(
+                            label: 'Room Name',
+                            ctrl: _roomCtrl,
                           ),
-                        const Text('CONNECT'),
-                      ],
-                    ),
+                        ),
+                      ElevatedButton(
+                        onPressed: _busy ? null : () => _connect(context),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            if (_busy)
+                              const Padding(
+                                padding: EdgeInsets.only(right: 10),
+                                child: SizedBox(
+                                  height: 15,
+                                  width: 15,
+                                  child: CircularProgressIndicator(
+                                    color: Colors.white,
+                                    strokeWidth: 2,
+                                  ),
+                                ),
+                              ),
+                            const Text('CONNECT'),
+                          ],
+                        ),
+                      ),
+                    ],
                   ),
-                ],
+                ),
               ),
             ),
-          ),
+            Positioned(
+              right: 0,
+              top: 0,
+              bottom: 0,
+              child: Container(
+                width: 200,
+                decoration: BoxDecoration(
+                  color: Colors.black.withOpacity(0.5),
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    bottomLeft: Radius.circular(20),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Text(
+                        'Active Rooms',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                        child: StreamBuilder<List<String>>(
+                      stream: getRoomList(),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasError) {
+                          return Text('Error: ${snapshot.error}');
+                        } else if (snapshot.connectionState ==
+                            asyncstate.ConnectionState.waiting) {
+                          return CircularProgressIndicator();
+                        } else {
+                          final roomList = snapshot.data;
+                          return ListView.builder(
+                            itemCount: roomList?.length,
+                            itemBuilder: (context, index) {
+                              return ListTile(
+                                title: Text(
+                                  roomList![index],
+                                  style: TextStyle(color: Colors.white),
+                                ),
+                                onTap: () {
+                                  setState(() {
+                                    _roomCtrl.text = roomList![index];
+
+                                    _isRoomNameInUrl = false;
+                                  });
+                                },
+                              );
+                            },
+                          );
+                        }
+                      },
+                    )),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       );
 }
